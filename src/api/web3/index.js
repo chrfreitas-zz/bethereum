609ab8332743dd2e24ac4637a7c35b8eaab3adb6
Christian Freitas <chrfreitas@gmail.com>
2019-07-18 19:18:46 -0300

    chore(prettier): add prettier

--- src/api/web3/index.js
+++ src/api/web3/index.js
@@ -1,17 +1,17 @@
-import Web3 from "web3";
+import Web3 from 'web3';
 
 const startWatch = () => {
-  const web3 = new Web3("wss://mainnet.infura.io/ws");
+  const web3 = new Web3('wss://mainnet.infura.io/ws');
 
-  web3.eth.subscribe("newBlockHeaders").on("data", block => {
+  web3.eth.subscribe('newBlockHeaders').on('data', block => {
     console.log(block);
   });
 };
 
 const getLast20Blocks = async amount => {
-  const web3 = new Web3("https://mainnet.infura.io/");
+  const web3 = new Web3('https://mainnet.infura.io/');
 
-  const block = await web3.eth.getBlock("latest");
+  const block = await web3.eth.getBlock('latest');
 
   for (let i = 0; i < amount; i++) {
     const x = await web3.eth.getBlock(block.number - i);
@@ -20,9 +20,9 @@ const getLast20Blocks = async amount => {
 };
 
 const getLastBlock = async () => {
-  const web3 = new Web3("https://mainnet.infura.io/");
+  const web3 = new Web3('https://mainnet.infura.io/');
 
-  const block = await web3.eth.getBlock("latest");
+  const block = await web3.eth.getBlock('latest');
   return block;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker';
 
-const App = () => "Hey";
-ReactDOM.render(<App />, document.getElementById("root"));
+const App = () => 'Hey';
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
