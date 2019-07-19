11611e361053a0f6389bdefaab4876ba3f10eae6
Christian Freitas <chrfreitas@gmail.com>
2019-07-18 22:28:22 -0300

    chore(Publish): create publish environment and start the process after build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 
-const App = () => 'Hey';
+const App = () => 'HI!';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
