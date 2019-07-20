8877fb8d8e0ba04efe7f42ad65c8e3a046353b9b
Christian Freitas <chrfreitas@gmail.com>
2019-07-20 11:35:33 -0300

    feature(Main): add header

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -5,6 +5,7 @@ import styles from './Main.scss';
 
 const Main = () => (
   <div className="main">
+    <h3 className="main__header">Ethereum Blockchain</h3>
     <div className="main__column">
       <BlockList />
     </div>
